4c5e49193608d70dc04133bf614155478261062a
mayursaini-pursuit <mayur_s@pursuitsoftware.biz>
2023-08-04 15:38:40 +0530

    commit

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:simple_grouped_listview/simple_grouped_listview.dart';
+import 'package:flutter/material.dart';
+import 'package:grouped_list/grouped_list.dart';
 
 class HomePage extends StatefulWidget{
 
@@ -23,9 +24,50 @@ class _HomePageState extends State<HomePage>{
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('GroupedListView.list()')),
-      body: ListView(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Grouped List View Example'),
+        ),
+        body: GroupedListView<dynamic, String>(
+          elements: _elements,
+          groupBy: (element) => element['group'],
+          groupComparator: (value1, value2) => value2.compareTo(value1),
+          itemComparator: (item1, item2) =>
+              item1['name'].compareTo(item2['name']),
+          order: GroupedListOrder.DESC,
+          useStickyGroupSeparators: true,
+          groupSeparatorBuilder: (String value) => Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              value,
+              textAlign: TextAlign.center,
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          itemBuilder: (c, element) {
+            return Card(
+              elevation: 8.0,
+              margin:
+              const EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+              child: SizedBox(
+                child: ListTile(
+                  contentPadding: const EdgeInsets.symmetric(
+                      horizontal: 20.0, vertical: 10.0),
+                  leading: const Icon(Icons.account_circle),
+                  title: Text(element['name']),
+                  trailing: const Icon(Icons.arrow_forward),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 
